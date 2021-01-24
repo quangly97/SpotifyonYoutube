45519b5b7af8d451dbc823dc8f077dee3a603d11
Kojo Obeng-Arhin <KwadwoArhin@hotmail.com>
2021-01-24 02:36:18 -0500

    add npm run build script to package.json

---
+++ spotify/build/precache-manifest.5b8ef31fbf7c43395b4d9a782909bd29.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d87362717092ccc30325ab9d0470656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b4b9877e0904183d186",
+    "url": "/static/css/2.5180387a.chunk.css"
+  },
+  {
+    "revision": "a8ab0cec8580c36f6811",
+    "url": "/static/css/main.7c8d1618.chunk.css"
+  },
+  {
+    "revision": "0b4b9877e0904183d186",
+    "url": "/static/js/2.f80dfed8.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/static/js/2.f80dfed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ab0cec8580c36f6811",
+    "url": "/static/js/main.e154b579.chunk.js"
+  },
+  {
+    "revision": "618a94e805e6ab69a130",
+    "url": "/static/js/runtime-main.d41ee8e9.js"
+  },
+  {
+    "revision": "5dcecaa697bb13386b161d300ff31c11",
+    "url": "/static/media/dynamic.5dcecaa6.jpg"
+  },
+  {
+    "revision": "9ac61d34a6e5d1605d785a9b9811847f",
+    "url": "/static/media/green-left-icon-arrow-left.9ac61d34.png"
+  },
+  {
+    "revision": "ca4c559e5b5dd000ab6c4d09de5e6f40",
+    "url": "/static/media/monkey-with-headphones.ca4c559e.jpg"
+  },
+  {
+    "revision": "df22d92977af7ec68ed88d5a3f486d84",
+    "url": "/static/media/spotify-youtube.df22d929.jpg"
+  }
+]);
