0ef94ce9e6b1988e2957e5e276ba36bb2fc2fd46
Kojo Obeng-Arhin <kwadwoarhin@hotmail.com>
2021-09-04 05:41:05 -0400

    Removed animate.css link

--- src/components/PlaylistButton/PlaylistButton.js
+++ src/components/PlaylistButton/PlaylistButton.js
@@ -1,30 +1,30 @@
-import React, { Component} from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Spotify from "spotify-web-api-js";
-import {Link} from 'react-router-dom';
-import {Button} from "react-bootstrap";
-import css from "./PlaylistButton.module.css"
+// import React, { Component} from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Spotify from "spotify-web-api-js";
+// import {Link} from 'react-router-dom';
+// import {Button} from "react-bootstrap";
+// import css from "./PlaylistButton.module.css"
 
-const spotifyWebApi = new Spotify();
+// const spotifyWebApi = new Spotify();
 
-class PlaylistButton extends Component {
-    constructor(props) {
-        super(props);
-        this.playlistID = this.props.id;
-        this.playlistName = this.props.name;
-    }
-    render () {
-        return (
-            <div className="playlist-button-and-image-container">
-                <Link to= {{
-                    pathname: "/youtube",
-                    playlistID: this.playlistID,
-                    }}>
-                    <Button className="playlist-button">{this.playlistName}</Button>
-                    <img className="playlist-image" src={this.props.image} alt={this.playlistName}/>
-                </Link>
-            </div>
-        )
-    }
-}
-export default PlaylistButton
+// class PlaylistButton extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.playlistID = this.props.id;
+//         this.playlistName = this.props.name;
+//     }
+//     render () {
+//         return (
+//             <div className="playlist-button-and-image-container">
+//                 <Link to= {{
+//                     pathname: "/youtube",
+//                     playlistID: this.playlistID,
+//                     }}>
+//                     <Button className="playlist-button">{this.playlistName}</Button>
+//                     <img className="playlist-image" src={this.props.image} alt={this.playlistName}/>
+//                 </Link>
+//             </div>
+//         )
+//     }
+// }
+// export default PlaylistButton
